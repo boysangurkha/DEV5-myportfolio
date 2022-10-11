725b20b8279a2b64e58f790e91d9b861c96c5fcd
boysangurkha <r0797383@student.thomasmore.be>
2022-10-11 21:13:35 +0200

    Docs: Made LAB3 file and copied what we made together in class but used another Weather API

---
+++ LAB3/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+// type="module" -> mogelijkheid om classes te schrijven en te importen en exporten
+import Weather from "./src/Weather.js";
+const weather = new Weather("35c8a70b92fd436696c10c618f137739");
+
+
+
---
+++ LAB3/src/Weather.js
@@ -0,0 +1,41 @@
+export default class Weather{
+    constructor(api_key){
+        this.apiKey = api_key;
+        // console.log("Weather class initialized" + this.apiKey);
+        this.getLocation();
+    }
+
+    // Get user's location
+    getLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
+        }else{
+            console.log("Geolocation is not supported by this browser");
+        }
+    }
+
+
+    getWeather(position){
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+
+        const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${this.apiKey}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                localStorage.setItem("weather", JSON.stringify(data));
+                console.log(data);
+                this.displayWeather(data);
+            });
+
+    }
+
+    displayWeather(data){
+        const temp = data.data[0].app_temp;
+        document.querySelector(".weather__temp").innerText = temp + "°C";
+
+        const weather = data.data[0].city_name;
+        document.querySelector(".weather__city").innerText = weather;
+    }
+}
