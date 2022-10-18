1bedfc0027f77714891437138caef405e4ffedd0
boysangurkha <r0797383@student.thomasmore.be>
2022-10-18 13:23:16 +0200

    Docs: Made a class for document settings

--- LAB4/main.js
+++ LAB4/main.js
@@ -1,37 +1,14 @@
 import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Document } from './src/Document.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-
-//add ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-scene.add(directionalLight);
-directionalLight.position.x = 1;
-directionalLight.position.y = 2;
-directionalLight.position.z = 4;
-
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
+const document = new Document();
+const scene = document.scene;
+const camera = document.camera;
+const renderer = document.renderer;
 
 function animate() {
   requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //rotate house
 
   renderer.render( scene, camera );
 };
---
+++ LAB4/src/Document.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+export class Document{
+    constructor(){
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( this.renderer.domElement );    
+        
+        this.camera.position.z = 5;
+        this.camera.position.y = 2;
+        this.camera.position.x = 2;
+    }
+}
+
